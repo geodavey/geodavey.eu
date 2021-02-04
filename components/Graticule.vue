58400bed582b73ec1390a712d52c937c147debf8
1papaya <1love1papaya@gmail.com>
2021-02-04 20:44:26 +0000

    add Graticule BG component

---
+++ components/Graticule.vue
@@ -0,0 +1,59 @@
+<template>
+  <svg
+    class="graticule-bg"
+    xmlns="http://www.w3.org/2000/svg"
+  ></svg>
+</template>
+
+<script lang="ts">
+import { geoPath, geoTransverseMercator, geoGraticule } from 'd3-geo';
+import { interval } from 'd3-timer';
+import { select } from 'd3-selection';
+
+export default {
+  data () {
+    return {
+      nativeWidth: 4096
+    };
+  },
+  mounted () {
+    const svg = select(this.$el)
+      .attr('width', this.$data.nativeWidth)
+      .attr('height', this.$data.nativeWidth)
+      .attr(
+        'viewBox',
+        `0 0 ${this.$data.nativeWidth} ${this.$data.nativeWidth}`
+      );
+
+    const proj = geoTransverseMercator()
+      .scale(this.$data.nativeWidth / 2)
+      .translate([this.$data.nativeWidth / 2, this.$data.nativeWidth / 2])
+      .center([0, 90])
+      .rotate([0, 0, 0]);
+
+    const path = geoPath().projection(proj);
+    const graticule = geoGraticule().step([12, 15]);
+
+    svg
+      .append('path')
+      .datum(graticule)
+      .attr('class', 'graticule')
+      .attr('d', path)
+      .style('fill', 'none')
+      .style('stroke', 'rgba(0,0,0,0.3)')
+      .attr('stroke-width', '2px');
+  }
+};
+</script>
+
+<style>
+.graticule-bg {
+  @apply fixed left-0;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 200vw;
+  height: 100vh;
+  z-index: -10;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,33 @@
 <template>
   <div class="container">
-    <div>
-      <Logo width=300 />
+    <div class="fade-in">
+      <Logo width="300" />
+      <Graticule />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
-export default Vue.extend({})
+export default Vue.extend({});
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
+.fade-in {
+  animation: 5s appear;
+  animation-delay: 0.5s;
+  margin: auto;
 }
-*/
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
 body {
   background: #fff;
-  }
+  font-family: verdana, tahoma, sans-serif;
+}
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -28,35 +35,5 @@ body {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
